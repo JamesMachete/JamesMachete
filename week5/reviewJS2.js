483ae9939650c711645a794d83ac2cbaa31ecb58
JamesMachete <64309010035@tnk.ac.th>
2022-12-07 14:36:16 +0700

    Create reviewJS2.js

---
+++ week5/reviewJS2.js
@@ -0,0 +1,10 @@
+let arr = ["JavaScript", 2565,3.71, true]
+console.log(typeof(arr))
+
+console.log(typeof(arr)[2])
+
+console.log(arr.length)
+for (i = 0 ; i < arr.length; i++){                 
+    const element = arr [i];
+    console.log(element)
+}
