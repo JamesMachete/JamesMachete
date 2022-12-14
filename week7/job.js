0389dbd6d4b0f8e9f4081101ac7ba1531985ab27
JamesMachete <64309010035@tnk.ac.th>
2022-12-14 14:43:40 +0700

    Create job.js

---
+++ week7/job.js
@@ -0,0 +1,75 @@
+let employee = [
+    {
+        "ID": 30001,
+        "Name":"นางปรียาดา ตั้งทิพย์",
+        "Department": "บุคคล",
+        "Position":"HR",
+        "Salary":48000,
+        "Bonus":1.4
+    },
+    {
+        "ID": 30002,
+        "Name":"นายณัฏฐ์ พงษ์วิโรจน์",
+        "Department": "ไอที",
+        "Position":"System Engineer",
+        "Salary":57000,
+        "Bonus":1.8
+    },
+    {
+        "ID": 30003,
+        "Name":"นายภูวนัย พัฒนปรีชา",
+        "Department": "การเงิน",
+        "Position":"Account",
+        "Salary":38000,
+        "Bonus":1.15
+    },
+    {
+        "ID": 30004,
+        "Name":"นางสาวเจนจิรา วงศ์สุรวัฒนา",
+        "Department": "บุคคล",
+        "Position":"HR",
+        "Salary":55000,
+        "Bonus":1.4
+    },
+    {
+        "ID": 30005,
+        "Name":"นายพนาวัฒน์ กลิ่นโพธิ์",
+        "Department": "บุลคล",
+        "Position":"HR",
+        "Salary":13000,
+        "Bonus":0.8
+    },
+    {
+        "ID": 30006,
+        "Name":"นายธนารีย์ ชัยเจริญ",
+        "Department": "การเงิน",
+        "Position":"Account",
+        "Salary":41000,
+        "Bonus":1.8
+    },
+    {
+        "ID": 30007,
+        "Name":"นายนิมมาน สยนานนท์",
+        "Department": "บุลคล",
+        "Position":"HR",
+        "Salary":19000,
+        "Bonus":1.4
+    },
+    {
+        "ID": 30008,
+        "Name":"นายกีรติ มากมี",
+        "Department": "การเงิน",
+        "Position":"Account",
+        "Salary":46000,
+        "Bonus":0.8
+    },
+    {
+        "ID": 30009,
+        "Name":"นางสาววรรณิศา โตศิลา",
+        "Department": "ไอที",
+        "Position":"System Engineer",
+        "Salary":28000,
+        "Bonus":1.15
+    }
+
+]
